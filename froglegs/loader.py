867e7c5ad22cd4005d4cd278a60018226c6a3dd5
María A. Matienzo <matienzo@berkeley.edu>
2025-04-17 09:18:36 -0700

    Add progress bar and basic interface to create a similarity query

--- froglegs/loader.py
+++ froglegs/loader.py
@@ -4,9 +4,11 @@ import typer
 from pathlib import Path
 from typing import List, Optional
 
+from langchain_core.vectorstores import InMemoryVectorStore
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_ollama import OllamaEmbeddings
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+from rich.progress import Progress, SpinnerColumn, TextColumn
 
 from pdffile import PDFFile
 
@@ -53,22 +55,43 @@ def main(
     """
     Process a set of PDF files.
     """
+    embeddings = OllamaEmbeddings(model=embedding_model)
+    vector_store = InMemoryVectorStore(embeddings)
+
     print("🐸 froglegs")
 
     if filepaths is None:
         filepaths = []
 
-    embeddings = OllamaEmbeddings(model=embedding_model)
+    with Progress(
+        SpinnerColumn(),
+        TextColumn("[progress.description]{task.description}"),
+        transient=True,
+    ) as progress:
+        progress.add_task(description="Loading %s..." % filepaths[0], total=None)
+        loader = PyPDFLoader(filepaths[0]) # hack :/
+        docs = loader.load()
 
-    loader = PyPDFLoader(filepaths[0]) # hack :/
-    docs = loader.load()
-    text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=1000, chunk_overlap=200, add_start_index=True
-    )
-    all_splits = text_splitter.split_documents(docs)
+        progress.add_task(description="Chunking %s..." % filepaths[0], total=None)
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=1000, chunk_overlap=200, add_start_index=True
+        )
+        all_splits = text_splitter.split_documents(docs)
+        rich.print(f"Returned {len(all_splits)} chunks for {filepaths[0]}.")
 
-    rich.print(len(all_splits))
-    rich.print(all_splits[3])
+        progress.add_task(description="Creating embeddings ...", total=None)
+        vector_1 = embeddings.embed_query(all_splits[0].page_content)
+        rich.print(f"Generating vectors of length {len(vector_1)}.")
+
+        progress.add_task(description="Storing vectors ...", total=None)
+        ids = vector_store.add_documents(documents=all_splits)
+
+    query = typer.prompt("What's your question?")
+    query_vector = embeddings.embed_query(query)
+    results = vector_store.similarity_search_by_vector(query_vector)
+
+    for result in results:
+        rich.print(result)
 
 if __name__ == "__main__":
     app()
